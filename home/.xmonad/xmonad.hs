0bb496978203cf5b1c0344232a8eb005a2a317fb
AitorATuin <atuin.dev@gmail.com>
2016-03-10 23:11:37 +0100

    First attemp of a functional xmonad

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,12 +1,34 @@
 import XMonad
-
-main = do
-  xmonad $ defaultConfig
-    { terminal    = myTerminal
-    , modMask     = myModMask
-    , borderWidth = myBorderWidth
-    }
-
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+ 
 myTerminal    = "urxvtc"
 myModMask     = mod4Mask -- Win key or Super_L
-myBorderWidth = 3
+myBorderWidth = 1
+
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+ 
+main = do
+    xmproc <- spawnPipe "/home/eof/.cabal/bin/xmobar /home/eof/.xmonad/xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+                        <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = myModMask
+        , terminal = myTerminal
+        , borderWidth = myBorderWidth
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
